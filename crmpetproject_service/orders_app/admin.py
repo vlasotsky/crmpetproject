d20b12a7fb14d84d8af0c72b187c3176302ad9ff
vlasotsky <v.lasotsky@gmail.com>
2024-04-02 15:29:34 +0200

    feat: add search for admin panel and useful descriptions upon creation

--- crmpetproject_service/orders_app/admin.py
+++ crmpetproject_service/orders_app/admin.py
@@ -5,27 +5,87 @@ from .models import Order, CoffeeMaker, Customer, CoffeeMakerInUse
 
 
 class CoffeemakerAdmin(admin.ModelAdmin):
+    search_fields = ("manufacturer", "model")
     list_display = ("id", "manufacturer", "model")
 
 
 class CustomerAdmin(admin.ModelAdmin):
+    search_fields = ("name", "addr")
     list_display = ("id", "name", "addr")
 
 
 class CoffeeMakerInUseAdmin(admin.ModelAdmin):
-    list_display = ("id", "serial_number", "customer_id",
-                    "coffeemaker_id", "status")
+    
+    def my_customer(self, obj):
+        return obj.customer.name
+    
+    def my_coffeemaker_model(self, obj):
+        return obj.coffeemaker.model
+
+    def my_coffeemaker_manufacturer(self, obj):
+        return obj.coffeemaker.manufacturer
+    
+    my_customer.short_description = "Client"
+    my_coffeemaker_model.short_description = "Model"
+    my_coffeemaker_manufacturer.short_description = "Brand"
+
+    search_fields = ("serial_number", "coffeemaker__manufacturer")
+    raw_id_fields = ("customer", "coffeemaker")
+    list_display = (
+        "id",
+        "my_coffeemaker_model",
+        "my_coffeemaker_manufacturer",
+        "serial_number",
+        "status"
+    )
+    
+    list_display = ("id", "serial_number", "customer_id", "coffeemaker_id", "status")
 
 
 class OrderAdmin(admin.ModelAdmin):
+
+    def my_coffeemaker_manufacturer(self, obj):
+        return obj.coffeemaker.manufacturer
+        
+    def my_coffeemaker_model(self, obj): 
+        return obj.coffeemaker.model
+
+    def my_customer_name(self, obj):
+        return obj.customer.name 
+
+    def my_customer_addr(self, obj): 
+        return obj.customer.name
+    
+    def my_serial_number(self, obj): 
+        return obj.coffeemaker.serial_number
+    
+    my_coffeemaker_manufacturer.short_description = "Brand"
+    my_coffeemaker_model.short_description = "Model"
+    my_customer_name.short_description = "Client"
+    my_customer_addr.short_description = "Address"
+    my_serial_number.short_description = "S/n"
+
     list_display = (
-        'customer_id',
-        "coffeemaker_id",
+        "id",
+        "my_serial_number",
+        "my_coffeemaker_model",
+        "my_coffeemaker_manufacturer",
+        "my_customer_name",
+        "my_customer_addr",
         "date",
         "last_updated_date",
         "status",
     )
 
+    search_fields = (
+        "coffeemakerinuse__customer__name",
+        "coffeemakerinuse__id",
+        "coffeemakerinuse__serial_number",
+        "coffeemakerinuse__coffeemaker__model",
+        "coffeemakerinuse__coffeemaker__manufacturer"
+    )
+
+    raw_id_fields = ("coffeemaker",)
 
 admin.site.register(CoffeeMaker, CoffeemakerAdmin)
 admin.site.register(Customer, CustomerAdmin)
--- crmpetproject_service/orders_app/models.py
+++ crmpetproject_service/orders_app/models.py
@@ -11,7 +11,7 @@ class CoffeeMaker(models.Model):
 
     class Meta:
         db_table = "coffeemakers"
-        verbose_name = "New coffeemaker"
+        verbose_name = "coffeemaker"
         verbose_name_plural = "Available coffeemakers"
 
     manufacturer = models.TextField(verbose_name="Manufacturer")
@@ -25,8 +25,8 @@ class Customer(models.Model):
 
     class Meta:
         db_table = "customers"
-        verbose_name = "New customer"
-        verbose_name_plural = "Customers' details"
+        verbose_name = "customer"
+        verbose_name_plural = "Customer info"
 
     name = models.TextField(verbose_name="Name")
     addr = models.TextField(verbose_name="Address")
@@ -39,20 +39,27 @@ class CoffeeMakerInUse(models.Model):
 
     class Meta:
         db_table = "coffeemakers_in_use"
-        verbose_name = "New coffeemaker"
+        verbose_name = "Currently in use"
         verbose_name_plural = "Coffeemakers currently in use"
 
     serial_number = models.TextField(verbose_name="Serial number")
+    
     customer = models.ForeignKey(
-        Customer, on_delete=models.RESTRICT, verbose_name="Owner ID"
+        Customer, 
+        on_delete=models.RESTRICT, 
+        verbose_name="Owner ID"
     )
+
     coffeemaker = models.ForeignKey(
-        CoffeeMaker, on_delete=models.RESTRICT, verbose_name="Coffeemaker ID"
+        CoffeeMaker, 
+        on_delete=models.RESTRICT, 
+        verbose_name="Coffeemaker ID"
     )
+
     status = models.TextField(verbose_name="Ownership status")
 
     def __str__(self) -> str:
-        return f"{self.serial_number}: {self.status}"
+        return f"{self.serial_number}, {self.coffeemaker.model}: {self.status}"
 
 
 class Order(models.Model):
@@ -66,25 +73,38 @@ class Order(models.Model):
 
     class Meta:
         db_table = "orders"
-        verbose_name = "New order"
+        verbose_name = "order"
         verbose_name_plural = "Orders"
 
     customer = models.ForeignKey(
-        Customer, on_delete=models.RESTRICT, verbose_name="Customer ID"
+        Customer, 
+        on_delete=models.RESTRICT, 
+        verbose_name="Customer ID"
     )
+
     coffeemaker = models.ForeignKey(
-        CoffeeMaker, on_delete=models.RESTRICT, verbose_name="Coffeemaker ID"
+        CoffeeMakerInUse, 
+        on_delete=models.RESTRICT, 
+        verbose_name="Coffeemaker ID"
     )
-    date = models.DateTimeField(verbose_name="Date and time created", auto_now_add=True)
+
+    date = models.DateTimeField(
+        verbose_name="Date and time created", 
+        auto_now_add=True
+    )
 
     last_updated_date = models.DateTimeField(
-        verbose_name="Last updated", blank=True, null=True
+        verbose_name="Last updated", 
+        blank=True, 
+        null=True
     )
-    status = models.TextField(verbose_name="Order status", choices=statuses)
 
-    def save(
-        self, *args, **kwargs
-    ):  # redefining function to have the last updated date save
+    status = models.TextField(
+        verbose_name="Order status", 
+        choices=statuses
+    )
+
+    def save(self, *args, **kwargs):
         self.last_updated_date = datetime.now()
         super().save(*args, **kwargs)
 
