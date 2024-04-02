0cdf9a2346620e75c4c7af9cd48e2302c604e647
vlasotsky <v.lasotsky@gmail.com>
2024-04-02 12:31:17 +0200

    fix: change models, make choices for status

--- crmpetproject_service/orders_app/models.py
+++ crmpetproject_service/orders_app/models.py
@@ -1,8 +1,6 @@
 import datetime
 from typing import Iterable
 from django.db import models
-from django.forms import ValidationError
-from django.utils.translation import gettext_lazy
 
 # Create your models here.
 
@@ -34,7 +32,7 @@ class Customer(models.Model):
     addr = models.TextField(verbose_name="Address")
 
     def __str__(self) -> str:
-        return f"{self.name}"
+        return f"{self.name} with address: {self.addr}"
 
 
 class CoffeeMakerInUse(models.Model):
@@ -45,51 +43,50 @@ class CoffeeMakerInUse(models.Model):
         verbose_name_plural = "Coffeemakers currently in use"
 
     serial_number = models.TextField(verbose_name="Serial number")
-    customer_id = models.ForeignKey(
+    customer = models.ForeignKey(
         Customer, on_delete=models.RESTRICT, verbose_name="Owner ID"
     )
-    coffeemaker_id = models.ForeignKey(
+    coffeemaker = models.ForeignKey(
         CoffeeMaker, on_delete=models.RESTRICT, verbose_name="Coffeemaker ID"
     )
     status = models.TextField(verbose_name="Ownership status")
 
     def __str__(self) -> str:
-        return f"{self.serial_number} {self.status}"
-
-
-# func to validate values of status variable to avoid values out of context
-def validate_status(status: str):
-
-    if status not in ["sold", "returned", "guarantee repair", "waiting for payment"]:
-        raise ValidationError(
-            gettext_lazy('%(status)s is out of valid status options.'),
-            params={'status': status},
-        )
+        return f"{self.serial_number}: {self.status}"
 
 
 class Order(models.Model):
+
+    statuses = (
+        ("sold", "sold"),
+        ("returned", "returned"),
+        ("guarantee repair", "warranty repair"),
+        ("waiting for payment", "not payed"),
+    )
 
     class Meta:
         db_table = "orders"
         verbose_name = "New order"
         verbose_name_plural = "Orders"
 
-    customer_id = models.ForeignKey(
+    customer = models.ForeignKey(
         Customer, on_delete=models.RESTRICT, verbose_name="Customer ID"
     )
-    coffeemaker_id = models.ForeignKey(
+    coffeemaker = models.ForeignKey(
         CoffeeMaker, on_delete=models.RESTRICT, verbose_name="Coffeemaker ID"
     )
-    date = models.DateTimeField(
-        verbose_name="Date and time created", auto_now_add=True)
+    date = models.DateTimeField(verbose_name="Date and time created", auto_now_add=True)
 
     last_updated_date = models.DateTimeField(
         verbose_name="Last updated", blank=True, null=True
     )
-    status = models.TextField(
-        verbose_name="Order status", validators=[validate_status])
+    status = models.TextField(verbose_name="Order status", choices=statuses)
 
-    # redefining function to have the last updated date save
-    def save(self, *args, **kwargs):
+    def save(
+        self, *args, **kwargs
+    ):  # redefining function to have the last updated date save
         self.last_updated_date = datetime.now()
         super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return f"{self.id}, {self.coffeemaker}"
